d16e01fc25ce1005c5c882aa986a073230cfb51a
rosenrose <crazytempler@gmail.com>
2025-01-06 17:38:07 +0900

    stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,140 +9,66 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
 
   @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void addCount(int cnt) {
+    setState(() {
+      counter += cnt;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 40,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 60,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text(
-                          "Hey, Flutter",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          "Welcome back",
-                          style: TextStyle(
-                            color: Colors.white60,
-                            fontSize: 16,
-                          ),
-                        ),
-                      ],
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      addCount(1);
+                    },
+                    icon: const Icon(
+                      Icons.add_circle_rounded,
+                      size: 70,
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  "Total Balance",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 22,
                   ),
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                Text(
-                  "\$5,194,482",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 48,
-                    fontWeight: FontWeight.w600,
+                  IconButton(
+                    onPressed: () {
+                      addCount(-1);
+                    },
+                    icon: const Icon(
+                      Icons.remove_circle_rounded,
+                      size: 70,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Button(
-                      text: "Transfer",
-                      textColor: Colors.black,
-                      bgColor: Color(0xFFF2B33A),
-                    ),
-                    Button(
-                      text: "Request",
-                      textColor: Colors.white,
-                      bgColor: Color(0xFF1F2123),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      "Wallets",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Text(
-                      "View All",
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 2,
-                ),
-                CurrencyCard(
-                  currencyName: "Euro",
-                  currencyCode: "EUR",
-                  currencyAmt: 6.428,
-                  currencyIcon: Icons.euro_rounded,
-                  order: 1,
-                ),
-                CurrencyCard(
-                  currencyName: "Dollar",
-                  currencyCode: "USD",
-                  currencyAmt: 10.998,
-                  currencyIcon: Icons.attach_money_rounded,
-                  order: 2,
-                  isLightMode: true,
-                ),
-                CurrencyCard(
-                  currencyName: "Bitcoin",
-                  currencyCode: "BTC",
-                  currencyAmt: 9.785,
-                  currencyIcon: Icons.currency_bitcoin_rounded,
-                  order: 3,
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
