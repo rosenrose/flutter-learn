7f131816e95441e5f3db9788d3eab82bd58cc299
rosenrose <crazytempler@gmail.com>
2025-01-07 16:50:38 +0900

    Pomodoro app

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_learn/widgets/Button.dart';
-import 'package:flutter_learn/widgets/currency_card.dart';
+import 'package:flutter_learn/screens/home_screen.dart';
 
 void main() {
-  var nico = Player(name: "nico");
-
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool isShowTitle = true;
-
-  void toggleTitle() {
-    setState(() {
-      isShowTitle = !isShowTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,69 +15,15 @@ class _AppState extends State<App> {
 
     return MaterialApp(
       theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFE7626C),
         textTheme: const TextTheme(
-          titleLarge: TextStyle(
-            color: Colors.red,
-            fontSize: 30,
+          headlineMedium: TextStyle(
+            color: Color(0xFF232B55),
           ),
         ),
+        cardColor: const Color(0xFFF4EDDB),
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFFF4EDDB),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              isShowTitle ? const MyLargeTitle() : const Text("Nothing"),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.remove_red_eye_sharp),
-                iconSize: 50,
-              )
-            ],
-          ),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  void initState() {
-    super.initState();
-    print("initState");
-  }
-
-  @override
-  void dispose() {
-    print("dispose");
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "My Large Title",
-      style: TextStyle(
-        fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
-      ),
-    );
-  }
-}
-
-class Player {
-  String name;
-
-  Player({required this.name});
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      body: Column(
+        children: [
+          Flexible(
+            flex: 2,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                "25:00",
+                style: TextStyle(
+                  color: Theme.of(context).cardColor,
+                  fontSize: 90,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 3,
+            child: Center(
+              child: IconButton(
+                icon: Icon(
+                  Icons.play_circle_outline,
+                  size: 120,
+                  color: Theme.of(context).cardColor,
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).cardColor,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Pomodoros",
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .color,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          "0",
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .textTheme
+                                .headlineMedium!
+                                .color,
+                            fontSize: 60,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
