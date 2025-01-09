563b9ecb5454958e61a0fbb8bbe7410f22559f51
rosenrose <crazytempler@gmail.com>
2025-01-09 17:15:34 +0900

    waitForWebtoons

--- lib/screens/webtoon.dart
+++ lib/screens/webtoon.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/services/api_service.dart';
+import 'package:flutter_learn/models/webtoon.dart';
 
 class WebtoonApp extends StatelessWidget {
   const WebtoonApp({super.key});
@@ -12,12 +13,36 @@ class WebtoonApp extends StatelessWidget {
   }
 }
 
-class WebtoonScreen extends StatelessWidget {
+class WebtoonScreen extends StatefulWidget {
   const WebtoonScreen({super.key});
 
   @override
+  State<WebtoonScreen> createState() => _WebtoonScreenState();
+}
+
+class _WebtoonScreenState extends State<WebtoonScreen> {
+  List<WebtoonModel> webtoonModels = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    waitForWebtoons();
+  }
+
+  void waitForWebtoons() async {
+    webtoonModels = await ApiService.getTodayWebtoons();
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    ApiService().getTodayWebtoons();
+    // ApiService().getTodayWebtoons();
+    print(webtoonModels);
+    print(isLoading);
 
     return Scaffold(
       backgroundColor: Colors.white,
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,17 +3,18 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_learn/models/webtoon.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "/today";
-  List<WebtoonModel> webtoonModels = [];
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "/today";
 
-  Future<List<WebtoonModel>> getTodayWebtoons() async {
+  static Future<List<WebtoonModel>> getTodayWebtoons() async {
     final http.Response res = await http.get(Uri.parse("$baseUrl$today"));
 
     if (res.statusCode != 200) {
       throw Exception("Failed to load today's webtoons");
     }
 
+    final List<WebtoonModel> webtoonModels = [];
     final List<dynamic> webtoons = jsonDecode(res.body);
 
     for (var webtoon in webtoons) {
@@ -21,9 +22,5 @@ class ApiService {
     }
 
     return webtoonModels;
-  }
-
-  Future<int> a() {
-    return Future.value(2 + 2);
   }
 }
